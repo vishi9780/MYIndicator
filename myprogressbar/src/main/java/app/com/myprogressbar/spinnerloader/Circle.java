8d794f4200d90024168bc581452bd702970cd9bc
Android40 <vishalyadav.think360@gmail.com>
2018-03-12 17:33:56 +0530

    172820181203HRS

--- myprogressbar/src/main/java/app/com/myprogressbar/spinnerloader/Circle.java
+++
@@ -1,9 +0,0 @@
-package app.com.myprogressbar.spinnerloader;
-
-/***
- * Created by Vishal Yadav :- 131320180903HRS
- */
-public class Circle {
-    public float[] center;
-    public float radius;
-}
