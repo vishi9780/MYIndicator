ec1f7fa1210545c88b50dc492a9259f613bb9097
Android40 <vishalyadav.think360@gmail.com>
2018-07-13 12:40:57 +0530

    YummyToast Module added not tested yer

--- app/src/main/java/app/com/myindicator/StateSaveFragment.java
+++ app/src/main/java/app/com/myindicator/StateSaveFragment.java
@@ -5,8 +5,12 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import app.com.myprogressbar.toastcustomization.YummyToast;
 
 public class StateSaveFragment extends AppCompatActivity {
 
@@ -23,6 +27,17 @@ public class StateSaveFragment extends AppCompatActivity {
         btn_f1=(Button)findViewById(R.id.btn_f1);
         btn_f2=(Button)findViewById(R.id.btn_f2);
         framelt=(FrameLayout) findViewById(R.id.framelt);
+        btn_f1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //SUCCESS
+                //WARNING
+                //ERROR
+                //INFO
+                //DEFAULT
+                YummyToast.makeText(getApplicationContext(),"", YummyToast.LENGTH_SHORT,YummyToast.SUCCESS);
+            }
+        });
     }
 
 
