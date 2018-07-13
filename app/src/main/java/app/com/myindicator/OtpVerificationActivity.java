5124c0036b3fb686f66231cec91ad612a43bfc75
Android40 <vishalyadav.think360@gmail.com>
2018-07-13 17:19:08 +0530

    Added Pin model tested

---
+++ app/src/main/java/app/com/myindicator/OtpVerificationActivity.java
@@ -0,0 +1,89 @@
+package app.com.myindicator;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import app.com.myprogressbar.otpview.PinView;
+
+
+public class OtpVerificationActivity extends AppCompatActivity {
+    EditText ed;
+    TextView tv;
+    String otp_generated,contactNo,id1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_verification);
+        ed=(EditText)findViewById(R.id.otp);
+        tv=(TextView) findViewById(R.id.verify_otp);
+        final PinView pinView = findViewById(R.id.secondPinView);
+        pinView.setTextColor(
+                ResourcesCompat.getColor(getResources(), R.color.colorAccent, getTheme()));
+        pinView.setTextColor(
+                ResourcesCompat.getColorStateList(getResources(), R.color.text_colors, getTheme()));
+        pinView.setLineColor(
+                ResourcesCompat.getColor(getResources(), R.color.colorPrimary, getTheme()));
+        pinView.setLineColor(
+                ResourcesCompat.getColorStateList(getResources(), R.color.line_colors, getTheme()));
+        pinView.setItemCount(4);
+        pinView.setItemHeight(getResources().getDimensionPixelSize(R.dimen.pv_pin_view_item_size));
+        pinView.setItemWidth(getResources().getDimensionPixelSize(R.dimen.pv_pin_view_item_size));
+        pinView.setItemRadius(getResources().getDimensionPixelSize(R.dimen.pv_pin_view_item_radius));
+        pinView.setItemSpacing(getResources().getDimensionPixelSize(R.dimen.pv_pin_view_item_spacing));
+        pinView.setLineWidth(getResources().getDimensionPixelSize(R.dimen.pv_pin_view_item_line_width));
+        pinView.setAnimationEnable(true);// start animation when adding text
+        pinView.setCursorVisible(false);
+        pinView.setCursorColor(
+                ResourcesCompat.getColor(getResources(), R.color.line_selected, getTheme()));
+        pinView.setCursorWidth(getResources().getDimensionPixelSize(R.dimen.pv_pin_view_cursor_width));
+        pinView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Log.e("TAG", "onTextChanged() called with: s = [" + s + "], start = [" + start + "], before = [" + before + "], count = [" + count + "]");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+    @Override
+    public void onResume() {
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, new IntentFilter("otp"));
+        super.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+    }
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equalsIgnoreCase("otp")) {
+                final String message = intent.getStringExtra("message");
+
+                Log.e("48","<<<<>>>>"+message.substring(10));
+                ed.setText(message.substring(10,16));
+                // message is the fetching OTP
+            }
+        }
+    };
+}
