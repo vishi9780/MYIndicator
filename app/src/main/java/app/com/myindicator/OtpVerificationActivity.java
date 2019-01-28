1a26299bab692b792e9c297e67ac8130819e0369
vy9780 <tms.vishalyadav@gmail.com>
2019-01-28 18:48:44 +0530

    fix bugs of pinView

--- app/src/main/java/app/com/myindicator/OtpVerificationActivity.java
+++ app/src/main/java/app/com/myindicator/OtpVerificationActivity.java
@@ -16,7 +16,6 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.regex.Matcher;
@@ -27,8 +26,7 @@ import app.com.myindicator.expandabletxt.RecyclerViewModeActivity;
 import app.com.myprogressbar.customsplash.ParticleView;
 import app.com.myprogressbar.justification_txt_edit_txt.JustifiedTextView;
 import app.com.myprogressbar.mygradient.GradientLinearLayout;
-import app.com.myprogressbar.mygradient.GradientRelativeLayout;
-import app.com.myprogressbar.otpview.PinView;
+import app.com.myprogressbar.otpview.VerifyCodeView;
 import app.com.myprogressbar.otpview.otpCatcher.OnSmsCatchListener;
 import app.com.myprogressbar.otpview.otpCatcher.SmsVerifyCatcher;
 import app.com.myprogressbar.toastcustomization.YummyToast;
@@ -42,7 +40,7 @@ public class OtpVerificationActivity extends AppCompatActivity {
     private ParticleView prVw_particle;
     private SmsVerifyCatcher smsVerifyCatcher;
     GradientLinearLayout activity_otp_verifications;
-
+    VerifyCodeView verfiy_cview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,40 +49,11 @@ public class OtpVerificationActivity extends AppCompatActivity {
         tv=(TextView) findViewById(R.id.verify_otp);
         tv_txt=(JustifiedTextView) findViewById(R.id.tv_txt);
         tv_txt.setText("Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.");
-        final PinView pinView = findViewById(R.id.firstPinView);
+
         final ParticleView prVw_particle = findViewById(R.id.prVw_particle);
         prVw_particle.startAnim();
 
-        pinView.setTextColor(ResourcesCompat.getColor(getResources(), R.color.colorAccent, getTheme()));
-        pinView.setTextColor(ResourcesCompat.getColorStateList(getResources(), R.color.text_colors, getTheme()));
-        pinView.setLineColor(ResourcesCompat.getColor(getResources(), R.color.colorPrimary, getTheme()));
-        pinView.setLineColor(ResourcesCompat.getColorStateList(getResources(), R.color.line_colors, getTheme()));
-        pinView.setItemCount(4);
-        pinView.setItemHeight(getResources().getDimensionPixelSize(R.dimen.pv_pin_view_item_size));
-        pinView.setItemWidth(getResources().getDimensionPixelSize(R.dimen.pv_pin_view_item_size));
-        pinView.setItemRadius(getResources().getDimensionPixelSize(R.dimen.pv_pin_view_item_radius));
-        pinView.setItemSpacing(getResources().getDimensionPixelSize(R.dimen.pv_pin_view_item_spacing));
-        pinView.setLineWidth(getResources().getDimensionPixelSize(R.dimen.pv_pin_view_item_line_width));
-        pinView.setAnimationEnable(true);// start animation when adding text
-        pinView.setCursorVisible(false);
-        pinView.setCursorColor(ResourcesCompat.getColor(getResources(), R.color.line_selected, getTheme()));
-        pinView.setCursorWidth(getResources().getDimensionPixelSize(R.dimen.pv_pin_view_cursor_width));
-        pinView.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                Log.e("TAG", "onTextChanged() called with: s = [" + s + "], start = [" + start + "], before = [" + before + "], count = [" + count + "]");
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
+        verfiy_cview=(VerifyCodeView)findViewById(R.id.verfiy_cview);
 
         //init SmsVerifyCatcher
 
@@ -92,7 +61,7 @@ public class OtpVerificationActivity extends AppCompatActivity {
             @Override
             public void onSmsCatch(String message) {
                 String code = parseCode(message);//Parse verification code
-                pinView.setText(code);
+                verfiy_cview.setText(code);
                 //set code in edit text
                 //then you can send verification code to server
             }
